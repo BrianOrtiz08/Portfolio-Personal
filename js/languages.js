f8d9febc5bf33bfe40a350d60d56245edb0bd7db
Brian Ortiz <ortizb355@gmail.com>
2023-11-18 22:57:17 -0300

    Agregando main en los dos idiomas

--- js/languages.js
+++ js/languages.js
@@ -2,12 +2,22 @@ const lenguages = [
     {
         "projects": "Proyectos",
         "aboutMe": "Acerca de mi",
-        "contact": "Contacto"
+        "contact": "Contacto",
+        "name": "¡Hola! Mi nombre es Brian Ortiz",
+        "rol": "Desarrollador Web Front End & Diseñador UX",
+        "paragraph": "En constante aprendizaje sobre el Desarrollo Web. Con sólidos conocimientos en HTML, CSS y JavaScript, busco transformar ideas en interfaces intuitivas para brindar la mejor experiencia de usuario posible. Si buscas un Desarrollador Web comprometido y entusiasta, estoy listo para tu próximo proyecto.",
+        "cv": "Ver Curriculum Vitae",
+        "contact": "Contáctame"
     },
     {
         "projects": "Projects",
         "aboutMe": "About me",
-        "contact": "Contact"
+        "contact": "Contact",
+        "name": "Hello! My name is Brian Ortiz",
+        "rol": "Front End Web Developer & UX Designer",
+        "paragraph": "Constantly learning about Web Development. With solid knowledge of HTML, CSS and JavaScript, I seek to transform ideas into intuitive interfaces to provide the best user experience possible. If you are looking for a committed and enthusiastic Web Developer, I am ready for your next project.",
+        "cv": "See resume",
+        "contact": "Contact me"
     }
 ]
 
@@ -29,9 +39,23 @@ function changeLanguage(e){
     let aboutMeId = document.getElementById("aboutMeId")
     let contactId = document.getElementById("contactId")
 
+    let nameId = document.getElementById("nameId")
+    let rolId = document.getElementById("rolId")
+    let paragraphId = document.getElementById("paragraphId")
+
+    let cvId = document.getElementById("cvId")
+    let contactMeId = document.getElementById("contactMeId")
+
     projectsId.innerText = lenguages[e].projects
     aboutMeId.innerText = lenguages[e].aboutMe
     contactId.innerText = lenguages[e].contact
+
+    nameId.innerText = lenguages[e].name
+    rolId.innerText = lenguages[e].rol
+    paragraphId.innerText = lenguages[e].paragraph
+
+    cvId.innerText = lenguages[e].cv
+    contactMeId.innerText = lenguages[e].contact
 }
 
 if(localStorage.getItem("languageSpanish")){
