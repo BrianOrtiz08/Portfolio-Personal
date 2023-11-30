26dc318c599dab3859f5edfff97f673fbb33b182
Brian Ortiz <ortizb355@gmail.com>
2023-11-30 17:55:05 -0300

    Cambiando seccion Proyectos

--- js/projects.js
+++ js/projects.js
@@ -4,11 +4,12 @@ const projectsSites = [
         name: "Sitio #1",
         image: "projectOne",
         skills: {
-            one: "HTML",
-            two: "CSS",
-            three: "JS"
+            one: "html",
+            two: "css",
+            three: "js"
         },
-        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod tenetur expedita ratione accusamus vitae pariatur excepturi, nihil aspernatur qui dicta!",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod tenetu",
+        iconGitHub: "github",
         icon: "open",
         link: "https://www.google.com/"
     },
@@ -17,11 +18,12 @@ const projectsSites = [
         name: "Sitio #2",
         image: "projectTwo",
         skills: {
-            one: "HTML",
-            two: "CSS",
-            three: "JS"
+            one: "html",
+            two: "css",
+            three: "js"
         },
-        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod tenetur expedita ratione accusamus vitae pariatur excepturi, nihil aspernatur qui dicta!",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod tenetu",
+        iconGitHub: "github",
         icon: "open",
         link: "https://www.facebook.com/"
     },
@@ -30,11 +32,12 @@ const projectsSites = [
         name: "Sitio #3",
         image: "projectThree",
         skills: {
-            one: "HTML",
-            two: "CSS",
-            three: "JS"
+            one: "html",
+            two: "css",
+            three: "js"
         },
-        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod tenetur expedita ratione accusamus vitae pariatur excepturi, nihil aspernatur qui dicta!",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod tenetu",
+        iconGitHub: "github",
         icon: "open",
         link: "https://www.pinterest.com/"
     },
@@ -43,11 +46,12 @@ const projectsSites = [
         name: "Sitio #4",
         image: "projectFour",
         skills: {
-            one: "HTML",
-            two: "CSS",
-            three: "JS"
+            one: "html",
+            two: "css",
+            three: "js"
         },
-        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod tenetur expedita ratione accusamus vitae pariatur excepturi, nihil aspernatur qui dicta!",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod tenetu",
+        iconGitHub: "github",
         icon: "open",
         link: "https://www.twitter.com/"
     },
@@ -56,11 +60,12 @@ const projectsSites = [
         name: "Sitio #5",
         image: "projectFive",
         skills: {
-            one: "HTML",
-            two: "CSS",
-            three: "JS"
+            one: "html",
+            two: "css",
+            three: "js"
         },
-        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod tenetur expedita ratione accusamus vitae pariatur excepturi, nihil aspernatur qui dicta!",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod tenetu",
+        iconGitHub: "github",
         icon: "open",
         link: "https://www.linkedin.com/"
     },
@@ -69,11 +74,12 @@ const projectsSites = [
         name: "Sitio #6",
         image: "projectSix",
         skills: {
-            one: "HTML",
-            two: "CSS",
-            three: "JS"
+            one: "html",
+            two: "css",
+            three: "js"
         },
-        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod tenetur expedita ratione accusamus vitae pariatur excepturi, nihil aspernatur qui dicta!",
+        details: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod tenetu",
+        iconGitHub: "github",
         icon: "open",
         link: "https://www.instagram.com/"
     }
@@ -84,23 +90,61 @@ const projectsGridId = document.getElementById("projectsGridId")
 projectsSites.map((e) => {
     projectsGridId.innerHTML += `
         <div class="projectsCard">
-            <a href="${e.link}" class="cardTitle" target="_blank">${e.name}</a>
-
-            <img src="./img/${e.image}.jpg" alt="Imagen previa del sitio ${e.name}" class="cardImg">
-
-            <div class="cardSkills">
-                <p class="cardSkill">${e.skills.one}</p>
-                <p class="cardSkill">${e.skills.two}</p>
-                <p class="cardSkill">${e.skills.three}</p>
+            <div class="cardImg">
+                <img src="./img/${e.image}.jpg" alt="Imagen previa del sitio ${e.name}" class="cardImage">
             </div>
 
-            <p class="cardDetails">${e.details}</p>
+            <div class="cardDetails">
+                <a href="${e.link}" class="cardTitle" target="_blank">${e.name}</a>
+                <p class="cardDescription">${e.details}</p>
+        
+                <div class="cardSkills">
+                    <img src="./img/${e.skills.one}.svg" alt="" class="cardSkill">
+                    <img src="./img/${e.skills.two}.svg" alt="" class="cardSkill">
+                    <img src="./img/${e.skills.three}.svg" alt="" class="cardSkill">
+                </div>
 
-            <div class="cardOpen">
-                <a href="${e.link}" target="_blank">
-                    <img src="./img/${e.icon}.svg" alt="Icono de "open in new tab"" class="cardOpenImg">
-                </a>
+                <div class="cardButtons">
+                    <a href="" class="cardButton">
+                        <p class="cardCode">Code</p>
+                        <img src="./img/${e.iconGitHub}.svg" alt="" class="imgCardCodeImg">
+                    </a>
+            
+                    <a href="" class="cardButton">
+                        <p class="cardSite">Live</p>
+                        <img src="./img/${e.icon}.svg" alt="Icono de "open in new tab"" class="imgCardSiteImg">
+                    </a>
+                </div>
             </div>
         </div>
     `
 })
+
+
+
+
+
+
+// projectsSites.map((e) => {
+//     projectsGridId.innerHTML += `
+//         <div class="projectsCard">
+//             <a href="${e.link}" class="cardTitle" target="_blank">${e.name}</a>
+
+//             <img src="./img/${e.image}.jpg" alt="Imagen previa del sitio ${e.name}" class="cardImg">
+
+//             <div class="cardSkills">
+//                 <p class="cardSkill">${e.skills.one}</p>
+//                 <p class="cardSkill">${e.skills.two}</p>
+//                 <p class="cardSkill">${e.skills.three}</p>
+//             </div>
+
+//             <p class="cardDetails">${e.details}</p>
+
+//             <div class="cardOpen">
+//                 <a href="${e.link}" target="_blank">
+//                     <img src="./img/${e.icon}.svg" alt="Icono de "open in new tab"" class="cardOpenImg">
+//                 </a>
+//             </div>
+//         </div>
+//     `
+// })
