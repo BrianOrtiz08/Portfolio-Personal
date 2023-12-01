a95f0ae48a6775cc1f475991e15276a9366d1e60
Brian Ortiz <ortizb355@gmail.com>
2023-12-01 16:47:39 -0300

    Terminando detalles de main y projects

--- scripts/languages.js
+++ scripts/languages.js
@@ -6,9 +6,10 @@ const lenguages = [
 
         "name": "¡Hola! Mi nombre es Brian Ortiz",
         "rol": "Desarrollador Web Front End & Diseñador UX",
-        "paragraph": "En constante aprendizaje sobre el Desarrollo Web. Con sólidos conocimientos en HTML, CSS y JavaScript, busco transformar ideas en interfaces intuitivas para brindar la mejor experiencia de usuario posible. Si buscas un Desarrollador Web comprometido y entusiasta, estoy listo para tu próximo proyecto.",
+        "paragraph": "En constante aprendizaje sobre el Desarrollo Web y el Diseño UX/UI.",
+        "paragraphSecond": "Si buscas un persona comprometida y entusiasta, estoy listo para tu próximo proyecto.",
 
-        "cv": "Ver Curriculum Vitae",
+        "cv": "Ver Curriculum",
         "contact": "Contáctame",
 
         "sectionProjects": "Proyectos"
@@ -20,7 +21,8 @@ const lenguages = [
 
         "name": "Hello! My name is Brian Ortiz",
         "rol": "Front End Web Developer & UX Designer",
-        "paragraph": "Constantly learning about Web Development. With solid knowledge of HTML, CSS and JavaScript, I seek to transform ideas into intuitive interfaces to provide the best user experience possible. If you are looking for a committed and enthusiastic Web Developer, I am ready for your next project.",
+        "paragraph": "Constantly learning about Web Development and UX/UI Design.",
+        "paragraphSecond": "If you are looking for a committed and enthusiastic person, I am ready for your next project.",
 
         "cv": "See resume",
         "contact": "Contact me",
@@ -42,7 +44,61 @@ buttonEn.addEventListener("click", () => {
     changeLanguage(1)
 })
 
-function changeLanguage(e){
+function changeLanguage(e) {
+    let projectsGridId = document.getElementById("projectsGridId")
+    let idiomaStorage = ""
+    let detailsLenguage = ""
+    let textCodeLenguage = ""
+    let textVisitLenguage = ""
+    let idioma = ""
+
+    projectsGridId.innerHTML = ""
+    idiomaStorage = localStorage.getItem("languageSpanish") === "true" ? 1 : 0;
+
+    if (idiomaStorage == 1) {
+        idioma = "es"
+    } else {
+        idioma = "en"
+    }
+
+    projectsSites.forEach((e) => {
+        detailsLenguage = e.details[idioma]
+        textCodeLenguage = e.textCode[idioma]
+        textVisitLenguage = e.textVisit[idioma]
+
+        projectsGridId.innerHTML += `
+        <div class="projectsCard">
+            <div class="cardImg">
+                <img src="../img/projects/${e.image}.jpg" alt="Imagen previa del sitio ${e.name}" class="cardImage">
+         </div>
+
+            <div class="cardDetails">
+                <a href="${e.link}" class="cardTitle" target="_blank">${e.name}</a>
+                <p class="cardDescription" id="${e.id}">${detailsLenguage}</p>
+    
+                <div class="cardSkills">
+                    <img src="../img/projects/${e.skills.one}.svg" alt="" class="cardSkill">
+                    <img src="../img/projects/${e.skills.two}.svg" alt="" class="cardSkill">
+                    <img src="../img/projects/${e.skills.three}.svg" alt="" class="cardSkill">
+                </div>
+
+                <div class="cardButtons">
+                    <a href="${e.linkGitHub}" class="cardButton" target="_blank">
+                        <p class="cardCode">${textCodeLenguage}</p>
+                        <img src="../img/projects/${e.iconGitHub}.svg" alt="" class="imgCardCodeImg">
+                    </a>
+        
+                    <a href="${e.link}" class="cardButton" target="_blank">
+                        <p class="cardSite">${textVisitLenguage}</p>
+                        <img src="../img/projects/${e.icon}.svg" alt="Icono de "open in new tab"" class="imgCardSiteImg">
+                    </a>
+                </div>
+            </div>
+        </div>
+    `
+    })
+
+
     let projectsId = document.getElementById("projectsId")
     let aboutMeId = document.getElementById("aboutMeId")
     let contactId = document.getElementById("contactId")
@@ -50,6 +106,7 @@ function changeLanguage(e){
     let nameId = document.getElementById("nameId")
     let rolId = document.getElementById("rolId")
     let paragraphId = document.getElementById("paragraphId")
+    let paragraphIdSecond = document.getElementById("paragraphIdSecond")
 
     let cvId = document.getElementById("cvId")
     let contactMeId = document.getElementById("contactMeId")
@@ -63,6 +120,7 @@ function changeLanguage(e){
     nameId.innerText = lenguages[e].name
     rolId.innerText = lenguages[e].rol
     paragraphId.innerText = lenguages[e].paragraph
+    paragraphIdSecond.innerText = lenguages[e].paragraphSecond
 
     cvId.innerText = lenguages[e].cv
     contactMeId.innerText = lenguages[e].contact
@@ -70,13 +128,13 @@ function changeLanguage(e){
     projectsTitleId.innerText = lenguages[e].sectionProjects
 }
 
-if(localStorage.getItem("languageSpanish")){
-    if(localStorage.getItem("languageSpanish") === "true"){
+if (localStorage.getItem("languageSpanish")) {
+    if (localStorage.getItem("languageSpanish") === "true") {
         changeLanguage(0)
-    }else{
+    } else {
         changeLanguage(1)
     }
-}else{
+} else {
     localStorage.setItem("languageSpanish", "true")
     changeLanguage(0)
 }
--- scripts/projects.js
+++ scripts/projects.js
@@ -8,11 +8,22 @@ const projectsSites = [
             two: "css",
             three: "js"
         },
-        details: "Descripción de sitio",
+        details: {
+            es: "Descripción de sitio",
+            en: "Website description"
+        },
         iconGitHub: "github",
         icon: "open",
         link: "https://www.google.com/",
-        linkGitHub: "https://www.google.com/"
+        linkGitHub: "https://www.google.com/",
+        textCode: {
+            es: "Código",
+            en: "Code"
+        },
+        textVisit: {
+            es: "Visitar",
+            en: "Open"
+        }
     },
     {
         id: 2,
@@ -23,11 +34,22 @@ const projectsSites = [
             two: "css",
             three: "js"
         },
-        details: "Descripción de sitio",
+        details: {
+            es: "Descripción de sitio",
+            en: "Website description"
+        },
         iconGitHub: "github",
         icon: "open",
         link: "https://www.facebook.com/",
-        linkGitHub: "https://www.google.com/"
+        linkGitHub: "https://www.google.com/",
+        textCode: {
+            es: "Código",
+            en: "Code"
+        },
+        textVisit: {
+            es: "Visitar",
+            en: "Open"
+        }
     },
     {
         id: 3,
@@ -38,11 +60,22 @@ const projectsSites = [
             two: "css",
             three: "js"
         },
-        details: "Descripción de sitio",
+        details: {
+            es: "Descripción de sitio",
+            en: "Website description"
+        },
         iconGitHub: "github",
         icon: "open",
         link: "https://www.pinterest.com/",
-        linkGitHub: "https://www.google.com/"
+        linkGitHub: "https://www.google.com/",
+        textCode: {
+            es: "Código",
+            en: "Code"
+        },
+        textVisit: {
+            es: "Visitar",
+            en: "Open"
+        }
     },
     {
         id: 4,
@@ -53,11 +86,22 @@ const projectsSites = [
             two: "css",
             three: "js"
         },
-        details: "Descripción de sitio",
+        details: {
+            es: "Descripción de sitio",
+            en: "Website description"
+        },
         iconGitHub: "github",
         icon: "open",
         link: "https://www.twitter.com/",
-        linkGitHub: "https://www.google.com/"
+        linkGitHub: "https://www.google.com/",
+        textCode: {
+            es: "Código",
+            en: "Code"
+        },
+        textVisit: {
+            es: "Visitar",
+            en: "Open"
+        }
     },
     {
         id: 5,
@@ -68,11 +112,22 @@ const projectsSites = [
             two: "css",
             three: "js"
         },
-        details: "Descripción de sitio",
+        details: {
+            es: "Descripción de sitio",
+            en: "Website description"
+        },
         iconGitHub: "github",
         icon: "open",
         link: "https://www.linkedin.com/",
-        linkGitHub: "https://www.google.com/"
+        linkGitHub: "https://www.google.com/",
+        textCode: {
+            es: "Código",
+            en: "Code"
+        },
+        textVisit: {
+            es: "Visitar",
+            en: "Open"
+        }
     },
     {
         id: 6,
@@ -83,46 +138,21 @@ const projectsSites = [
             two: "css",
             three: "js"
         },
-        details: "Descripción de sitio",
+        details: {
+            es: "Descripción de sitio",
+            en: "Website description"
+        },
         iconGitHub: "github",
         icon: "open",
         link: "https://www.instagram.com/",
-        linkGitHub: "https://www.google.com/"
+        linkGitHub: "https://www.google.com/",
+        textCode: {
+            es: "Código",
+            en: "Code"
+        },
+        textVisit: {
+            es: "Visitar",
+            en: "Open"
+        }
     }
 ]
-
-const projectsGridId = document.getElementById("projectsGridId")
-
-projectsSites.map((e) => {
-
-    projectsGridId.innerHTML += `
-        <div class="projectsCard">
-            <div class="cardImg">
-                <img src="../img/projects/${e.image}.jpg" alt="Imagen previa del sitio ${e.name}" class="cardImage">
-            </div>
-
-            <div class="cardDetails">
-                <a href="${e.link}" class="cardTitle" target="_blank">${e.name}</a>
-                <p class="cardDescription" id="${e.id}">${e.details}</p>
-        
-                <div class="cardSkills">
-                    <img src="../img/projects/${e.skills.one}.svg" alt="" class="cardSkill">
-                    <img src="../img/projects/${e.skills.two}.svg" alt="" class="cardSkill">
-                    <img src="../img/projects/${e.skills.three}.svg" alt="" class="cardSkill">
-                </div>
-
-                <div class="cardButtons">
-                    <a href="${e.linkGitHub}" class="cardButton" target="_blank">
-                        <p class="cardCode">Code</p>
-                        <img src="../img/projects/${e.iconGitHub}.svg" alt="" class="imgCardCodeImg">
-                    </a>
-            
-                    <a href="${e.link}" class="cardButton" target="_blank">
-                        <p class="cardSite">Live</p>
-                        <img src="../img/projects/${e.icon}.svg" alt="Icono de "open in new tab"" class="imgCardSiteImg">
-                    </a>
-                </div>
-            </div>
-        </div>
-    `
-})
